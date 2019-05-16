687444e942105b5ade97e52c040be5288d05488f
Rodo1foBandeira <rodolfo_band@hotmail.com>
2019-05-16 18:16:55 -0300

    Teste Sorted

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
-import urllib.request, json
-from datetime import date
+import urllib.request, json, pytz
+from datetime import datetime, timedelta
+
+def toDate(dtStr):
+    return datetime(int(dtStr[0:4]), int(dtStr[5:7]), int(dtStr[8:10]), int(dtStr[11:13]), int(dtStr[14:16]), int(dtStr[17:19]), int(dtStr[20:24]), tzinfo=pytz.UTC)
 
 with urllib.request.urlopen("https://mdgateway04.easynvest.com.br/iwg/snapshot/?t=webgateway&c=5448062&q=WINM19") as url:
 	data = json.loads(url.read().decode())
@@ -9,16 +12,14 @@ with urllib.request.urlopen("https://mdgateway04.easynvest.com.br/iwg/snapshot/?
 
 	response = data['Value'][0]
 	for item in response['Ts']:
-		item['DT'] = self.toDate(item['DT'])
+		item['DT'] = toDate(item['DT'])
 		item['Br'] = int(item['Br'])
 		item['Sr'] = int(item['Sr'])
 		item['Q'] = int(item['Q'])
 		item['P'] = float(item['P'])
 
-	tst = sort(lambda x: x['DT'], response['Ts'])
+	response['Ts'][0]['DT'] = response['Ts'][0]['DT'] + timedelta(days=1)		
+	tst = sorted(response['Ts'], key=lambda x: x['DT'])
 
 	for item in tst:
 		print(str(item['DT']))
-
-def toDate(dtStr):
-    return datetime(int(dtStr[0:4]), int(dtStr[5:7]), int(dtStr[8:10]), int(dtStr[11:13]), int(dtStr[14:16]), int(dtStr[17:19]), int(dtStr[20:24]), tzinfo=pytz.UTC)
